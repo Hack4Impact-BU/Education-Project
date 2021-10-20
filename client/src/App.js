781fd8fbf41f3b74d40b07c9ba4268485457bd08
Cici Chen <iccchen@bu.edu>
2021-10-20 16:44:39 -0400

    node modules install

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,11 @@
 import React from 'react'
+import Button from '@mui/material/Button';
+
 function App() {
   return (
     <div>
       <h1>Education Project Front-End</h1>
+      <Button>Hello World!</Button>
     </div>
   );
 }
