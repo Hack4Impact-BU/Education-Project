8d30dd7b863ef665102d4dbb86da8a55eb1ce11c
Evan Hsu <evanlhsu@dhcp-wifi-8021x-155-41-122-88.bu.edu>
2021-10-27 17:16:31 -0400

    initial commit to branch

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import Button from '@mui/material/Button';
 import Homepage from './Homepage';
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+// import LocalizationProvider from '@mui/lab/LocalizationProvider';
 
 function App() {
   return (
     <div>
       <h1>Education Project Front-End</h1>
+      {/* <LocalizationProvider dateAdapter={AdapterDateFns}>...</LocalizationProvider> */}
       <Homepage></Homepage>
     </div>
   );
--- client/src/Homepage.js
+++ client/src/Homepage.js
@@ -1,71 +1,155 @@
 import React from 'react'
+import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid'
-import Box from '@mui/material/Box'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
-import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
-import Select from "@mui/material/Select"
 
+// import AdapterDateFns from '@mui/lab/AdapterDateFns';
+// import LocalizationProvider from '@mui/lab/LocalizationProvider';
+// import DatePicker from '@mui/lab/DatePicker';
+
+import TextField from '@mui/material/TextField'
+
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
 
 class Homepage extends React.Component {
     render() {
         return (
-            <Box sx = {{flexGrow: 1}}>
-            <Grid container spacing={2}>
-                <Grid item xs={4}>
-                    <div style={{backgroundColor:"lightGray"}}>Schedule Tutoring Session(s)</div>
-                </Grid>
-                <Grid item xs={4}>
-                <div style={{backgroundColor:"lightGray"}}>Log New or Scheduled Session(s)</div>
-                </Grid>
-                <Grid item xs={4}>
-                <div style={{backgroundColor:"lightGray"}}>Review Completed Tutoring Session(s)</div>
-                </Grid>
-
-                <Grid item xs={4}>
-                <Card>
-                    <CardContent style={{backgroundColor:"LightGray"}}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={6}>
+            <Box sx={{ flexGrow: 1 }}>
+                <Grid container spacing={2}>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent sx={{backgroundColor:'#F5F5F5'}}>
+                                <Typography sx={{textAlign: 'center'}}>Schedule Tutoring Session(s)</Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent sx={{backgroundColor:'#F5F5F5'}}>
+                                <Typography sx={{textAlign: 'center'}}>Log New or Scheduled Session(s)</Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent sx={{backgroundColor:'#F5F5F5'}}>
+                                <Typography sx={{textAlign: 'center'}}>Review Completed Tutoring Session(s)</Typography>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent sx={{backgroundColor:'#F5F5F5'}}>
                                 <Typography sx={{fontSize: 16}}>Schedule & Plan Session</Typography>
-                                <TextField id="outlined-basic" inputProps={{ style: {textAlign:'center'} }} label="yyyy-mm-dd" size="small" ></TextField>
-                            </Grid>
-                            <Grid item xs={6}>
-                                <Typography sx={{fontSize: 16}}>Student</Typography>
-                                <Select id="demo-simple-select" label="select from dropdown"></Select>
-                            </Grid>
-                        </Grid>
-                    
-                    </CardContent>
-                </Card>
+                                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                                    <Grid item xs={6}>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Tutoring Date</Typography>
+                                        <TextField fullWidth id="outlined-basic" inputProps={{ style: {textAlign:'center'} }} label="yyyy-mm-dd" size="small" ></TextField>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Student</Typography>
+                                        <FormControl fullWidth>
+                                            <InputLabel id="demo-simple-select-label">select from dropdown</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                label="Age"
+                                            >
+                                            <MenuItem value={10}>student 1</MenuItem>
+                                            <MenuItem value={20}>student 2</MenuItem>
+                                            <MenuItem value={30}>student 3</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Subject/Class</Typography>
+                                        <FormControl fullWidth>
+                                            <InputLabel id="demo-simple-select-label">select from dropdown</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                label="Age"
+                                            >
+                                            <MenuItem value={10}>class 1</MenuItem>
+                                            <MenuItem value={20}>class 2</MenuItem>
+                                            <MenuItem value={30}>class 3</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Notes</Typography>
+                                        <TextField fullWidth id="standard-basic" variant="standard" />
+                                        <Button sx={{alignItems: 'right'}} variant="contained">Submit</Button>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent sx={{backgroundColor:'#F5F5F5'}}>
+                                <Typography sx={{fontSize: 16, textAlign: 'center'}}>Schedule & Plan Session</Typography>
+                                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                                    <Grid item xs={6}>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Tutoring Date</Typography>
+                                        <TextField fullWidth id="outlined-basic" inputProps={{ style: {textAlign:'center'} }} label="yyyy-mm-dd" size="small" ></TextField>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Student</Typography>
+                                        <FormControl fullWidth>
+                                            <InputLabel id="demo-simple-select-label">select from dropdown</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                label="Age"
+                                            >
+                                            <MenuItem value={10}>student 1</MenuItem>
+                                            <MenuItem value={20}>student 2</MenuItem>
+                                            <MenuItem value={30}>student 3</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Time</Typography>
+                                        <TextField fullWidth id="standard-basic" label="hours" variant="standard" />
+                                        <TextField fullWidth id="standard-basic" label="minutes" variant="standard" />
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Notes</Typography>
+                                        <TextField fullWidth id="standard-basic" variant="standard" />
+                                        <Button sx={{alignItems: 'right'}} variant="contained">Submit</Button>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
+                    <Grid item xs={4}>
+                        <Card>
+                            <CardContent sx={{backgroundColor:'#F5F5F5'}}>
+                                <Typography sx={{fontSize: 16}}>Schedule & Plan Session</Typography>
+                                <Grid container spacing={2} alignItems="center" justifyContent="center">
+                                    <Grid item xs={6}>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Tutoring Date</Typography>
+                                        <TextField fullWidth id="outlined-basic" inputProps={{ style: {textAlign:'center'} }} label="yyyy-mm-dd" size="small" ></TextField>
+                                    </Grid>
+                                    <Grid item xs={6}>
+                                        <Typography sx={{fontSize: 16, textAlign: 'center'}}>Student</Typography>
+                                        <FormControl fullWidth>
+                                            <InputLabel id="demo-simple-select-label">select from dropdown</InputLabel>
+                                            <Select
+                                                labelId="demo-simple-select-label"
+                                                id="demo-simple-select"
+                                                label="Age"
+                                            >
+                                            <MenuItem value={10}>student 1</MenuItem>
+                                            <MenuItem value={20}>student 2</MenuItem>
+                                            <MenuItem value={30}>student 3</MenuItem>
+                                            </Select>
+                                        </FormControl>
+                                    </Grid>
+                                </Grid>
+                            </CardContent>
+                        </Card>
+                    </Grid>
                 </Grid>
-                <Grid item xs={4}>
-                <Card>
-                    <CardContent style={{backgroundColor:"LightGray"}}>
-                        <Grid container spacing={2}>
-                            <Grid item xs={6}>
-                                <Typography sx={{fontSize: 16}}>Scheduled</Typography>
-                                <TextField id="outlined-basic" inputProps={{ style: {textAlign:'center'} }} label="yyyy-mm-dd" size="small" ></TextField>
-                            </Grid>
-                            <Grid item xs={6}>
-                                
-                                <Typography sx={{fontSize: 16}}>Student</Typography>
-                                <Select id="demo-simple-select" label="select from dropdown"></Select>
-                            </Grid>
-                        </Grid>
-                    
-                    </CardContent>
-                </Card>
-                </Grid>
-
-                <Grid item xs={4}>
-                <Card>
-                    <CardContent style={{backgroundColor:"LightGray"}}>tt</CardContent>
-                </Card>
-                </Grid>
-
-            </Grid>
             </Box>
         )
     }
