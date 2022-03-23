9378d6aecc30e72b49fae0ccdbf23bf6b2689be2
Shasta Narayanan <snaray02@gmail.com>
2022-03-23 16:56:09 -0400

    withAllFields

--- client/src/components/RegistrationForms/TutorRegistration.js
+++ client/src/components/RegistrationForms/TutorRegistration.js
@@ -66,7 +66,7 @@ export default class TutorRegistration extends Component {
 
         const { subjects, languages, gradeLevels } = this.state;
 
-        const description = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
+        const description = `Test ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et 
             dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
             Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. 
             Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.`
