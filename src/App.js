580c4b7f46294478a35369f53326bf995c6d48b3
egreven <egreven@bu.edu>
2022-04-08 14:05:17 -0400

    testing dynamodb making note

--- src/App.js
+++ src/App.js
@@ -21,13 +21,40 @@ import BackgroundCheck from './components/Forms/BackgroundCheck';
 import { Authenticator } from '@aws-amplify/ui-react'
 
 import '@aws-amplify/ui-react/styles.css';
+import { listNotes } from './graphql/queries';
+import { createNote as createNoteMutation, deleteNote as deleteNoteMutation } from './graphql/mutations';
 
-
+const initialFormState = { name: '', description: '' }
 export default function App() {
+  const [notes, setNotes] = useState([]);
+  const [formData, setFormData] = useState(initialFormState);
+
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  async function fetchNotes() {
+    const apiData = await API.graphql({ query: listNotes });
+    setNotes(apiData.data.listNotes.items);
+  }
+
+  async function createNote() {
+    if (!formData.name || !formData.description) return;
+    await API.graphql({ query: createNoteMutation, variables: { input: formData } });
+    setNotes([ ...notes, formData ]);
+    setFormData(initialFormState);
+  }
+
+  async function deleteNote({ id }) {
+    const newNotesArray = notes.filter(note => note.id !== id);
+    setNotes(newNotesArray);
+    await API.graphql({ query: deleteNoteMutation, variables: { input: { id } }});
+  }
   return (
     <Authenticator socialProviders={['amazon', 'apple', 'facebook', 'google']}>
       {({ signOut, user}) => (
     <div className="App">
+      
      <Router>
       <Navbar />
       <div className="content">      
@@ -56,7 +83,28 @@ export default function App() {
       <h1>Hello {user.username}</h1>
           <button onClick={signOut}>Sign out</button>
       </div>
-    
+      <h1>My Notes App</h1>
+      <input
+        onChange={e => setFormData({ ...formData, 'name': e.target.value})}
+        placeholder="Note name"
+        value={formData.name}
+      />
+      <input
+        onChange={e => setFormData({ ...formData, 'description': e.target.value})}
+        placeholder="Note description"
+        value={formData.description}
+      />
+      <button onClick={createNote}>Create Note</button>
+      <div style={{marginBottom: 30}}>
+        {
+          notes.map(note => (
+            <div key={note.id || note.name}>
+              <h2>{note.name}</h2>
+              <p>{note.description}</p>
+              <button onClick={() => deleteNote(note)}>Delete note</button>
+            </div>
+          ))
+        } </div>
     </Router>
    
     </div> ) } </Authenticator>
