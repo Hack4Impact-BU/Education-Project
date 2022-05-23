173c41c639b2977259716c3d8c68ab0222422dde
egreven <egreven@bu.edu>
2022-05-23 16:26:08 -0400

    working on tutorial

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import Navbar from './components/Navbar'
 import TutorRegistration from './components/RegistrationForms/TutorRegistration';
